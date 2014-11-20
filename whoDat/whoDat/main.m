b2b562a0ff6c45f0b8baf70c29326d2b8e15bbb4
mp-uforia <mpandeyadav@gmail.com>
2014-11-19 18:57:42 -0800

    Initial checkin

---
+++ whoDat/whoDat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  whoDat
+//
+//  Created by Moni on 11/19/14.
+//  Copyright (c) 2014 Uforia Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
